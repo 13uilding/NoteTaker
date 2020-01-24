e7bf08baf8dbc852759137f9e5c6aaaeaff86037
deebs <hereisdanbrown@gmail.com>
2020-01-23 22:38:07 -0700

    Added uuid package to take care of unique ids

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,22 @@
 const path = require("path");
 const fs = require("fs");
+const uuidv4 = require('uuid/v4');
 
 
 module.exports = function(app) {
 
     // Need to determine what req.body will look like
     app.post("/api/notes", function(req, res) {
-        const notesArr = require("../db/db.json");
         let file = path.join(__dirname , "../db/db.json");
+        const notesArr = require(file);
         let note = req.body;
-        note.id = Object.keys(notesArr).length;
+        note.id = uuidv4();
         let notes = [];
         if (Object.keys(notesArr).length !== 0) {
-            notes = notesArr;
+            notes = notesArr.slice();
         };
         notes.push(note);
+        // Await functionality?
         fs.writeFile(file, JSON.stringify(notes, null, 2), err => {
             if (err) throw err;
         });
@@ -28,19 +30,18 @@ module.exports = function(app) {
         let file = path.join(__dirname , "../db/db.json");
         const notesArr = require(file);
         let notes = notesArr.slice()
-        let deleteNoteId = Number(req.params.id);
+        let deleteNoteId = req.params.id;
         for (let i = 0; i < notes.length; i++) {
             if (notes[i].id === deleteNoteId) {
                 notes.splice(i, 1);
                 break;
             };
         };
+        // Await functionality?
         fs.writeFile(file, JSON.stringify(notes, null, 2), err => {
             if (err) throw err;
+            res.send(notes);
         });
-        console.log(req.params.id);
-
-        res.end(notes);
     })
 
 
